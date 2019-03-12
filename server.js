f1d1cd4a2fd596acec8bebed26bd76673aec73ff
Michael Shakmakov <Michael_Shakmakov@epam.com>
2019-03-12 22:50:00 +0400

    .env, passport, session init

--- server.js
+++ server.js
@@ -3,19 +3,30 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
+const session = require('express-session');
+const passport = require('passport');
+const dotenv = require('dotenv');  
 const app = express();
 
+dotenv.config();
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log("Listening on port " + (process.env.PORT || 3000));
 });
