eb72269d91ea2030b1ee422981aee1e37a197b3f
Michael Shakmakov <Michael_Shakmakov@epam.com>
2019-03-31 20:42:50 +0400

    moving passport config to auth module

--- auth.js
+++ auth.js
@@ -7,62 +7,62 @@ const GitHubStrategy = require('passport-github').Strategy;
 const dotenv = require('dotenv');
 dotenv.config();
 
-passport.serializeUser((user, done) => { 
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  db.collection('users').findOne(
-      {_id: new ObjectID(id)},
-      (err, doc) => {
-        done(null, doc);
-      }
-  );
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    db.collection('users').findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "https://swamp-rhythm.glitch.me/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      //Database logic here with callback containing our user object
-      db.collection('socialusers').findAndModify(
-        {id: profile.id},
-        {},
-        {$setOnInsert:{
-            id: profile.id,
-            name: profile.displayName || 'New User',
-            photo: profile.photos[0].value || '',
-            email: profile.emails[0].value || 'No public email',
-            created_on: new Date(),
-            provider: profile.provider || ''
-        },$set:{
-            last_login: new Date()
-        },$inc:{
-            login_count: 1
-        }},
-        {upsert:true, new: true},
+module.exports = function (app, db) {
+  passport.serializeUser((user, done) => { 
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    db.collection('users').findOne(
+        {_id: new ObjectID(id)},
         (err, doc) => {
-            return cb(null, doc.value);
+          done(null, doc);
         }
     );
-  }
-));
-
-module.exports = function (app, db) {
+  });
+  
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      db.collection('users').findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+  
+  passport.use(new GitHubStrategy({
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "https://swamp-rhythm.glitch.me/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing our user object
+        db.collection('socialusers').findAndModify(
+          {id: profile.id},
+          {},
+          {$setOnInsert:{
+              id: profile.id,
+              name: profile.displayName || 'New User',
+              photo: profile.photos[0].value || '',
+              email: profile.emails[0].value || 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || ''
+          },$set:{
+              last_login: new Date()
+          },$inc:{
+              login_count: 1
+          }},
+          {upsert:true, new: true},
+          (err, doc) => {
+              return cb(null, doc.value);
+          }
+      );
+    }
+  ));
   app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
